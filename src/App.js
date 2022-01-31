396b82819ea108cb7cec2cdaf7e6201c6b64372c
“Evgeny <ahotickkk@mail.ru>
2022-01-31 19:38:58 +0300

    class Component in react

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 // Use state возращает массив из 2-х объектов
 import React, { useState } from "react";
+import ClassCounter from "./Components/ClassCounter";
 import Counter from "./Components/Counter";
 
 function App() {  
+
   return (
     <div className="App">
-       <Counter />
+       <ClassCounter />
     </div>
   );
+  
 }
 
 export default App;
