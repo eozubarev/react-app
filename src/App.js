fa9e48a5ea8b43fb0a13bc3c8e74089163669a2b
“Evgeny <ahotickkk@mail.ru>
2022-02-01 22:43:08 +0300

    update react app

--- src/App.js
+++ src/App.js
@@ -2,12 +2,25 @@
 import React, { useState } from "react";
 import ClassCounter from "./Components/ClassCounter";
 import Counter from "./Components/Counter";
+import './styles/App.css';
 
 function App() {  
 
   return (
     <div className="App">
-       <ClassCounter />
+
+       <div className="post">
+          <div className="post__content">
+              <strong>1. Javascript</strong>
+              <div>
+                Javascript - язык программирования
+              </div>
+          </div>
+          <div className="post__btns">
+              <button>Удалить пост</button>
+          </div>
+       </div>
+       
     </div>
   );
   
